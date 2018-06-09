9e431bdd9169a992d4f37f43e486e3a9761d3f4f
sorenthesmed <andersen.soren.p@gmail.com>
2018-06-09 16:43:55 +0200

    Implemented mailing functionality, but it doesn't work. I suspect it's windows having trouble with TLS / SSL

---
+++ data/emailCreds.py
@@ -0,0 +1,6 @@
+
+def getEmail():
+    return "smedtechinfoservice@gmail.com"
+
+def getPass():
+    return "Pgbc8rpe7c7"
--- scripts/sendMail.py
+++ scripts/sendMail.py
@@ -1,17 +1,22 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from data.emailCreds import *
 
 msg = MIMEMultipart("alternative")
 msg["Subject"] = "Service update"
-msg["From"] = "smedtechinfoservice@gmail.com"
+msg["From"] = getEmail()
 msg["To"] = ", ".join(open("../data/dummyEmails"))
+# msg["To"] = "soer8617@stud.kea.dk" #Only for testing. The line above is the one to be used
 msg.preamble = "Service update"
 
 html = """\
 <html>
     <head></head>
     <body>
+        <p>We have recently detected unnatural activity from your LinkedIn profile.</p>
+        <p>We need you to prove you are the owner, so we can see if any other mismatching</p>
+        <p>IP's have tried to or have had access to your account.</p>
         Login here to verify you are the owner: <a href="https://www.sneakytime.com/rr/">LinkedIn.com</a>
     </body>
 </html>
@@ -19,6 +24,14 @@ html = """\
 
 msg.attach(html)
 
-s = smtplib.SMTP("localhost")
-s.sendmail()
-s.quit()
+try:
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(getEmail(), getPass())
+    server.sendmail(getEmail(), msg.as_string())
+    server.quit()
+    print("Succeeded in sending mail.")
+
+except:
+    print("Failed to send mail")
