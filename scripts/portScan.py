48bebc4173d50f8dc73b181597359461ebd13291
sorenthesmed <andersen.soren.p@gmail.com>
2018-06-09 11:18:19 +0200

    Created a portscanner

---
+++ scripts/portScan.py
@@ -0,0 +1,39 @@
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+
+# subprocess.call("clear", shell=True)
+
+remoteServer = input("Enter a remote host to scan: ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+
+print("-" * 40)
+print("begun scanning on: ", remoteServerIP)
+print("-" * 40)
+
+t1 = datetime.now()
+
+
+try:
+    for port in range(1, 1024):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print("Port {}:   Open".format(port))
+        sock.close()
+
+except socket.gaierror:
+    sys.exit("Hostname could not be resolved. Exiting")
+
+except socket.error:
+    sys.exit("Could not connect to server")
+
+
+t2 = datetime.now()
+
+totalTime = t2 - t1
+
+print("Scanning completed in: ", totalTime)
