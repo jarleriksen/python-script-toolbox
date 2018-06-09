ac1b52957476268dd152a12046dd0759e70fee07
Jarl Eriksen <jarl@civitas-software.com>
2018-06-09 10:51:34 +0200

    Added try except block

--- scripts/dictionary.py
+++ scripts/dictionary.py
@@ -1,18 +1,22 @@
 import hashlib
 
+# Leaked pastebin of random accounts
+# https://pastebin.com/Rdw5F4Qu
+
 hash = input("Enter your hash: ")
 passwords = "../data/rockyou.txt"
 
 
 def main():
-
-    with open(passwords) as password:
-        for line in password:
-            line = line.strip()
-            if hashlib.md5(line.encode('utf-8')).hexdigest() == hash:
-                print("The password is: %s" % line)
-                return ""
-    print("Failed to crack the file.")
+    try:
+        with open(passwords) as password:
+            for line in password:
+                line = line.strip()
+                if hashlib.md5(line.encode('utf-8')).hexdigest() == hash:
+                    print("The password is: %s" % line)
+                    return ""
+    except UnicodeDecodeError:
+        print("Failed to crack the password.")
 
 
 main()
