8a6063f7e2198ea20dba65564cfddbbf6db8a664
sorenthesmed <andersen.soren.p@gmail.com>
2018-06-10 12:55:12 +0200

    Added options to the menu

--- scripts/menu.py
+++ scripts/menu.py
@@ -2,5 +2,31 @@
 
 print("-------------------------------")
 print("Welcome to the ultimate hacking")
-print("/ reconnaisance tool!")
+print("/ reconnaissance tool!")
 print("-------------------------------")
+
+
+def printMenu():
+    print("What do you want to do?")
+
+    operation = input(">> ")
+
+    if operation == "1":
+        # webscraper
+        pass
+
+    elif operation == "2":
+        # portscan
+        pass
+
+    elif operation == "3":
+        # send mail
+        pass
+
+    elif operation == "4":
+        # dict attack
+        pass
+
+    else:
+        print("No menu entry with value: ", operation)
+        print("Try one of the listed menu options.")
